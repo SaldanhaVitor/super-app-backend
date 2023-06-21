113bd593c34c754813994c3aadd861618b5a3d19
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-20 22:01:01 -0300

    chore: create clients module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './modules/clients/clients.module';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
