d2fd42d116bc53f2c3d1f0f4b7afdf459af0f879
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-22 19:42:48 -0300

    chore: create wishlist module

---
+++ src/modules/wishlist/wishlist.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { WishlistService } from './wishlist.service';
+import { CreateWishlistDto } from './dto/create-wishlist.dto';
+import { UpdateWishlistDto } from './dto/update-wishlist.dto';
+
+@Controller('wishlist')
+export class WishlistController {
+  constructor(private readonly wishlistService: WishlistService) {}
+
+  @Post()
+  create(@Body() createWishlistDto: CreateWishlistDto) {
+    return this.wishlistService.create(createWishlistDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.wishlistService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.wishlistService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateWishlistDto: UpdateWishlistDto) {
+    return this.wishlistService.update(+id, updateWishlistDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.wishlistService.remove(+id);
+  }
+}
