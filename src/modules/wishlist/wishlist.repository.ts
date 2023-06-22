b64c716746a233c130ff7a6a8d819c8331bfb091
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-22 20:33:30 -0300

    refactor: remove unused dtos

--- src/modules/wishlist/wishlist.repository.ts
+++ src/modules/wishlist/wishlist.repository.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateWishlistDto } from './dto/create-wishlist.dto';
-import { UpdateWishlistDto } from './dto/update-wishlist.dto';
 import { WishlistResponseDto } from './dto/wishlist-response.dto';
 import { Wishlist } from './entities/wishlist.entity';
 import { v4 as uuidv4 } from 'uuid';
