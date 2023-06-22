1366480577a8020d6cad1c149f1b1fbc0cb56418
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-22 19:49:51 -0300

    feat: create wishlist entity

--- src/modules/wishlist/entities/wishlist.entity.ts
+++ src/modules/wishlist/entities/wishlist.entity.ts
@@ -1,1 +1,12 @@
-export class Wishlist {}
+export type Product = {
+  id: string;
+  title: string;
+  image: string;
+  price: number;
+  review?: number;
+};
+export class Wishlist {
+  id: string;
+  clientId: string;
+  products: Product[];
+}
