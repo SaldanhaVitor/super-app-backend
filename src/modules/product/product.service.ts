aa2e272074619ac10f833e4ba6a0183bb8fd4d26
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-25 00:58:45 -0300

    test: ensure list products

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -2,18 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { Product } from './entities/product.entity';
 import { HttpClientService } from '../shared/http-client/http-client.service';
 import { Constants } from '../../configs';
-import { ProductParser } from './helper/parser-product.helper';
+import {
+  AllProductsParser,
+  ProductParser,
+} from './helper/parser-product.helper';
 import ProductGenericException from './exceptions/product-generic-error.exception';
 
 @Injectable()
 export class ProductService {
   constructor(private readonly httpClient: HttpClientService) {}
+
   async getProductById(productId: string): Promise<Product> {
     try {
       const product = await this.httpClient.get(
         `${Constants.luizalabs.challengeApi}/${productId}`,
       );
       return ProductParser(product);
+    } catch (error) {
+      throw new ProductGenericException(error.message, error.response.status);
+    }
+  }
+
+  async listProducts(page: number): Promise<Product[]> {
+    try {
+      const products = await this.httpClient.get(
+        `${Constants.luizalabs.challengeApi}/?page=${page}`,
+      );
+      return AllProductsParser(products);
     } catch (error) {
       throw new ProductGenericException(error.message, error.response.status);
     }
