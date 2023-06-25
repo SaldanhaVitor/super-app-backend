b2b304879660c8ae9502c4d23e99dbedf6ec6bf2
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-25 01:13:43 -0300

    test: axios adapter tests

--- src/modules/axios-adapter/axios-adapter.spec.ts
+++ src/modules/axios-adapter/axios-adapter.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AxiosAdapter } from './axios.adapter';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { Constants } from '../../configs';
 
 describe('AxiosAdapter', () => {
   let service: AxiosAdapter;
@@ -14,5 +17,15 @@ describe('AxiosAdapter', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('get', () => {
+    it('should call an url by http get', async () => {
+      const mock = new MockAdapter(axios);
+      const data = { response: true };
+      mock.onGet(Constants.luizalabs.challengeApi).reply(200, data);
+      const response = await service.get(Constants.luizalabs.challengeApi);
+      expect(response).toBeDefined();
+    });
   });
 });
