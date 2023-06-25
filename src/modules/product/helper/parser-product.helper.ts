7078acb50b976135575abe02f11b165b16b5cca8
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-25 00:58:50 -0300

    feat: ensure list products

--- src/modules/product/helper/parser-product.helper.ts
+++ src/modules/product/helper/parser-product.helper.ts
@@ -1,11 +1,25 @@
 import { Product } from '../entities/product.entity';
 
-export const ProductParser = (data: any): Product => {
+export const ProductParser = (productResponse: any): Product => {
   return {
-    id: data.id ?? 'missing id',
-    title: data.title ?? 'missing title',
-    review: data?.review ?? 'missing review',
-    image: data.image ?? 'missing image',
-    price: data.price ?? 'missing price',
+    id: productResponse.id ?? 'missing id',
+    title: productResponse.title ?? 'missing title',
+    review: productResponse?.review ?? 'missing review',
+    image: productResponse.image ?? 'missing image',
+    price: productResponse.price ?? 'missing price',
   };
+};
+
+export const AllProductsParser = (allProductsResponse: any[]): Product[] => {
+  const products: Product[] = [];
+  for (const product of allProductsResponse) {
+    products.push({
+      id: product.id ?? 'missing id',
+      title: product.title ?? 'missing title',
+      review: product?.review ?? 'missing review',
+      image: product.image ?? 'missing image',
+      price: product.price ?? 'missing price',
+    });
+  }
+  return products;
 };
--- src/modules/product/product.service.spec.ts
+++ src/modules/product/product.service.spec.ts
@@ -6,6 +6,7 @@ import { PRODUCT_FOUND } from './__mocks__/product-found.mock';
 import { HttpClientModule } from '../shared/http-client/http-client.module';
 import { HttpClientService } from '../shared/http-client/http-client.service';
 import ProductGenericException from './exceptions/product-generic-error.exception';
+import { PRODUCTS } from './__mocks__/all-products';
 
 const mockEndpointGet = jest.fn();
 
@@ -57,6 +58,28 @@ describe('ProductService', () => {
       });
       const productId = uuidv4();
       await expect(service.getProductById(productId)).rejects.toThrow(
+        ProductGenericException,
+      );
+      expect(mockEndpointGet).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('List products', () => {
+    it('should list products', async () => {
+      mockEndpointGet.mockReturnValue(PRODUCTS);
+      const page = 1;
+      const products: Product[] = await service.listProducts(page);
+      expect(products).toBeDefined();
+      expect(products).toHaveLength(3);
+      expect(mockEndpointGet).toHaveBeenCalledTimes(1);
+    });
+    it('should throws when list products is not found', async () => {
+      mockEndpointGet.mockRejectedValue({
+        message: 'Product not found',
+        response: { status: 404 },
+      });
+      const page = 1;
+      await expect(service.listProducts(page)).rejects.toThrow(
         ProductGenericException,
       );
       expect(mockEndpointGet).toHaveBeenCalledTimes(1);
