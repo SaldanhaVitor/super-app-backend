9e50b1165a51eb2ee4210d9dfb6e98c71c023b23
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-25 01:22:26 -0300

    test: httpClientService tests

---
+++ src/modules/shared/http-client/http-client.service.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpClientService } from './http-client.service';
+import { AxiosAdapterModule } from '../../../modules/axios-adapter/axios-adapter.module';
+import { AxiosAdapter } from '../../../modules/axios-adapter/axios.adapter';
+import { Constants } from '../../../configs';
+
+const mockGetByAdapter = jest.fn();
+describe('HttpClientService', () => {
+  let service: HttpClientService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        {
+          global: true,
+          module: AxiosAdapterModule,
+          providers: [
+            {
+              provide: AxiosAdapter,
+              useValue: {
+                get: mockGetByAdapter,
+              },
+            },
+          ],
+        },
+      ],
+      providers: [HttpClientService],
+    }).compile();
+
+    service = module.get<HttpClientService>(HttpClientService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('get', () => {
+    it('Should call get by its adapter', async () => {
+      mockGetByAdapter.mockReturnValue(undefined);
+      await service.get(Constants.luizalabs.challengeApi);
+      expect(mockGetByAdapter).toHaveBeenCalledTimes(1);
+    });
+  });
+});
