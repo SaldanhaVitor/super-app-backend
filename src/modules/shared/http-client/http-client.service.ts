c9f87410a7511e86fa81528da3ec5db48582b74d
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-25 01:01:00 -0300

    refactor: httpClientService should implements httpClient

--- src/modules/shared/http-client/http-client.service.ts
+++ src/modules/shared/http-client/http-client.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { AxiosAdapter } from '../../../modules/axios-adapter/axios.adapter';
+import { HttpClient } from './interfaces/http-client.interface';
 
 @Injectable()
-export class HttpClientService {
+export class HttpClientService implements HttpClient {
   constructor(private readonly httpAdapter: AxiosAdapter) {}
 
   async get(url: string): Promise<any> {
