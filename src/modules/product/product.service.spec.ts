5afbda1f93cb0b096b1e6a8b7ac13e798da353a6
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-25 00:45:10 -0300

    test: ensure trows when product is not found

--- src/modules/product/product.service.spec.ts
+++ src/modules/product/product.service.spec.ts
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { PRODUCT_FOUND } from './__mocks__/product-found.mock';
 import { HttpClientModule } from '../shared/http-client/http-client.module';
 import { HttpClientService } from '../shared/http-client/http-client.service';
+import ProductGenericException from './exceptions/product-generic-error.exception';
 
 const mockEndpointGet = jest.fn();
 
@@ -47,6 +48,17 @@ describe('ProductService', () => {
       const productId = uuidv4();
       const product: Product = await service.getProductById(productId);
       expect(product).toBeDefined();
+      expect(mockEndpointGet).toHaveBeenCalledTimes(1);
+    });
+    it('should trows when product is not found', async () => {
+      mockEndpointGet.mockRejectedValue({
+        message: 'Product not found',
+        response: { status: 404 },
+      });
+      const productId = uuidv4();
+      await expect(service.getProductById(productId)).rejects.toThrow(
+        ProductGenericException,
+      );
       expect(mockEndpointGet).toHaveBeenCalledTimes(1);
     });
   });
