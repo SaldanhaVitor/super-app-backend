741860f3a4ae407999d4d4e1f42d958b46229342
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-25 22:55:34 -0300

    test: local strategy tests

--- src/modules/auth/strategies/local.strategy.spec.ts
+++ src/modules/auth/strategies/local.strategy.spec.ts
@@ -7,9 +7,12 @@ import { ClientsService } from '../../../modules/clients/clients.service';
 import { AuthController } from '../auth.controller';
 import { AuthService } from '../auth.service';
 import { JwtStrategy } from './jwt.strategy';
+import { EXISTENT_CLIENT_WITH_PASSWORD } from '../../../modules/clients/__mocks__/client-with-password';
+import UnauthorizedException from '../exception/unauthorized.exception';
 
 const mockSignJwt = jest.fn();
 const mockFindOneToAuthorize = jest.fn();
+const mockAuthServiceValidateClient = jest.fn();
 
 describe('LocalStrategy', () => {
   let strategy: LocalStrategy;
@@ -44,7 +47,16 @@ describe('LocalStrategy', () => {
         },
       ],
       controllers: [AuthController],
-      providers: [AuthService, LocalStrategy, JwtStrategy],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            validateClient: mockAuthServiceValidateClient,
+          },
+        },
+        LocalStrategy,
+        JwtStrategy,
+      ],
     }).compile();
 
     strategy = module.get<LocalStrategy>(LocalStrategy);
@@ -56,5 +68,28 @@ describe('LocalStrategy', () => {
 
   it('should be defined', () => {
     expect(strategy).toBeDefined();
+  });
+
+  describe('validate', () => {
+    it('should call validate method', async () => {
+      mockAuthServiceValidateClient.mockReturnValue(
+        EXISTENT_CLIENT_WITH_PASSWORD,
+      );
+      const email = 'any@mail.com';
+      const password = 'any_password';
+      const client = await strategy.validate(email, password);
+      expect(client).toBeDefined();
+      expect(mockAuthServiceValidateClient).toHaveBeenCalledTimes(1);
+    });
+    it('should throw unauthorized when client is invalid', async () => {
+      mockAuthServiceValidateClient.mockReturnValue(null);
+      const email = 'any@mail.com';
+      const password = 'any_password';
+      await expect(strategy.validate(email, password)).rejects.toThrow(
+        UnauthorizedException,
+      );
+
+      expect(mockAuthServiceValidateClient).toHaveBeenCalledTimes(1);
+    });
   });
 });
