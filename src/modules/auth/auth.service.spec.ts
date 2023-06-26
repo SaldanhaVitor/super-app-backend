a756264d1586b8b10f1360c842b66a071368eae9
SaldanhaVitor <vitorgtfo@gmail.com>
2023-06-25 22:27:02 -0300

    test: auth service tests

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -7,8 +7,11 @@ import { ClientsModule } from '../clients/clients.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { EXISTENT_CLIENT_WITH_PASSWORD } from '../clients/__mocks__/client-with-password';
+import { ClientsService } from '../clients/clients.service';
+import ClientNotFoundException from '../clients/exceptions/client-not-found.exception';
 
 const mockSignJwt = jest.fn();
+const mockFindOneToAuthorize = jest.fn();
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -29,7 +32,18 @@ describe('AuthService', () => {
             },
           ],
         },
-        ClientsModule,
+        {
+          global: true,
+          module: ClientsModule,
+          providers: [
+            {
+              provide: ClientsService,
+              useValue: {
+                findOneToAuthorize: mockFindOneToAuthorize,
+              },
+            },
+          ],
+        },
       ],
       controllers: [AuthController],
       providers: [AuthService, LocalStrategy, JwtStrategy],
@@ -51,6 +65,37 @@ describe('AuthService', () => {
       const jwtResponse = await service.login(EXISTENT_CLIENT_WITH_PASSWORD);
       expect(jwtResponse).toBeDefined();
       expect(mockSignJwt).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('validateClient', () => {
+    it('should validate client', async () => {
+      mockFindOneToAuthorize.mockReturnValue(EXISTENT_CLIENT_WITH_PASSWORD);
+      const client = await service.validateClient(
+        EXISTENT_CLIENT_WITH_PASSWORD.email,
+        EXISTENT_CLIENT_WITH_PASSWORD.password,
+      );
+      expect(client).toBeDefined();
+      expect(mockFindOneToAuthorize).toHaveBeenCalledTimes(1);
+    });
+    it('should return null when client is not found', async () => {
+      mockFindOneToAuthorize.mockRejectedValue(new ClientNotFoundException());
+      const client = await service.validateClient(
+        EXISTENT_CLIENT_WITH_PASSWORD.email,
+        EXISTENT_CLIENT_WITH_PASSWORD.password,
+      );
+      expect(client).toBeNull();
+      expect(mockFindOneToAuthorize).toHaveBeenCalledTimes(1);
+    });
+    it('should return null when client password is not equal to login password', async () => {
+      mockFindOneToAuthorize.mockReturnValue(EXISTENT_CLIENT_WITH_PASSWORD);
+      const anotherPassword = 'any_other_non_existent_password';
+      const client = await service.validateClient(
+        EXISTENT_CLIENT_WITH_PASSWORD.email,
+        anotherPassword,
+      );
+      expect(client).toBeNull();
+      expect(mockFindOneToAuthorize).toHaveBeenCalledTimes(1);
     });
   });
 });
